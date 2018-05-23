392256df9f557c95fd2a0ede940eb63c88b24cf5
Nannan Gu <396426364@qq.com>
2018-05-23 22:22:02 +1000

    map

--- src/main/java/mycontroller/Drive.java
+++ src/main/java/mycontroller/Drive.java
@@ -1,4 +1,26 @@
 package mycontroller;
 
+import utilities.Coordinate;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Drive {
+    private MyAIController controller;
+    private MapRecorder mapRecorder;
+    private Queue<Operation> operations;
+
+    public Drive(MyAIController controller,MapRecorder mapRecorder){
+        this.operations=new LinkedList<>();
+        this.controller=controller;
+        this.mapRecorder=mapRecorder;
+    }
+
+    public Queue<Operation> getOperations(ArrayList<Coordinate> path){
+
+    }
+
+
+
 }
--- src/main/java/mycontroller/MyAIController.java
+++ src/main/java/mycontroller/MyAIController.java
@@ -1,25 +1,37 @@
 package mycontroller;
 
 import controller.CarController;
+import org.omg.CORBA.CurrentOperations;
 import tiles.MapTile;
 import utilities.Coordinate;
 import world.Car;
 
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
 
 
 public class MyAIController extends CarController{
-//	private MyNavigateStrategy strategy ;
 	private MapRecorder mapRecorder;
+	private Drive drive;
+	private Path path;
+	private Queue<Operation> operations;
+	private Coordinate targetPosition;
+	private Coordinate currentPosition;
 
 
 	public MyAIController(Car car) {
 		super(car);
 		Coordinate co = new Coordinate((int)car.getX(),(int)car.getY());
-//		strategy = new MyNavigateStrategy(co);
 		mapRecorder = new MapRecorder();
 		mapRecorder.addPointsByCarView(car.getView());
+		drive=new Drive(this,mapRecorder);
+		path=new Path(mapRecorder);
+		operations=new LinkedList<>();
+		targetPosition=new Coordinate(this.getPosition());
+		currentPosition=new Coordinate(this.getPosition());
 	}
 
 	@Override
@@ -29,7 +41,20 @@ public class MyAIController extends CarController{
 		HashMap<Coordinate, MapTile> currentView = getView();
 		mapRecorder.addPointsByCarView(currentView);
 
-//		Operation operation = strategy.getOperation(mapRecorder.getMap(),this);
+		currentPosition=new Coordinate(this.getPosition());
+		if(targetPosition.equals(currentPosition)){
+			targetPosition=NextPositionFactory.chooseNextPositionStrategy(this).
+					getNextPosition(this.mapRecorder,this);
+		}
+
+		if(operations.size()==0){
+			operations=drive.getOperations(path.findPath(currentPosition,targetPosition));
+		}
+
+		handleOperation(operations.poll());
+
+
+
 	}
 
 	private void handleOperation(Operation operation){
