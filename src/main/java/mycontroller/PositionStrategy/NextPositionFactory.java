50dad459735d819bdde617de90b15d60e32701d3
kangnwh <hnzz.kn@gmail.com>
2018-05-24 21:36:45 +1000

    runnalbe

--- src/main/java/mycontroller/PositionStrategy/NextPositionFactory.java
+++ src/main/java/mycontroller/PositionStrategy/NextPositionFactory.java
@@ -3,10 +3,12 @@ package mycontroller.PositionStrategy;
 import controller.CarController;
 import mycontroller.MapRecorder;
 
+import static mycontroller.PositionStrategy.HealPositionStrategy.HEALTH_THRESHOLD;
+
 public class NextPositionFactory {
 
 	public static INextPositionStrategy chooseNextPositionStrategy(CarController car, MapRecorder mapRecorder) {
-		if (car.getHealth() < 30&&mapRecorder.hasHealthTrap()) {
+		if (car.getHealth() < HEALTH_THRESHOLD && mapRecorder.hasHealthTrap()) {
 			return new HealPositionStrategy();
 		} else if (car.getKey() != 1 && mapRecorder.keyFounded(car.getKey() - 1)) {
 			return new KeyPositionStrategy();
