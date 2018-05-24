ea0ee1f2c54beb8771a9ad147e5abac7a8a576e4
Nannan Gu <396426364@qq.com>
2018-05-24 20:22:58 +1000

    delete todo

--- src/main/java/mycontroller/PositionStrategy/NextPositionFactory.java
+++ src/main/java/mycontroller/PositionStrategy/NextPositionFactory.java
@@ -6,7 +6,7 @@ import mycontroller.MapRecorder;
 public class NextPositionFactory {
 
 	public static INextPositionStrategy chooseNextPositionStrategy(CarController car, MapRecorder mapRecorder) {
-		if (car.getHealth() < 30) {
+		if (car.getHealth() < 30&&mapRecorder.hasHealthTrap()) {
 			return new HealPositionStrategy();
 		} else if (car.getKey() != 1 && mapRecorder.keyFounded(car.getKey() - 1)) {
 			return new KeyPositionStrategy();
