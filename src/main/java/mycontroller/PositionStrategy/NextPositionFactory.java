cab939f6162e459fcbb404a07eff7a92e8d2b16f
Nannan Gu <396426364@qq.com>
2018-05-25 20:48:45 +1000

    delete todo

--- src/main/java/mycontroller/PositionStrategy/NextPositionFactory.java
+++ src/main/java/mycontroller/PositionStrategy/NextPositionFactory.java
@@ -5,6 +5,11 @@ import mycontroller.MapRecorder;
 
 import static mycontroller.PositionStrategy.HealPositionStrategy.HEALTH_THRESHOLD;
 
+/**
+ * The factory is used to return different position strategies according in different condition
+ * If health is little, the car will go to the healthTrap,if keys is found but not get, the car will go to get the key,
+ * if all keys are get, the car will go to the exit, else the car will explore the maze.
+ */
 public class NextPositionFactory {
 
 	private static INextPositionStrategy healPositionStrategy = new HealPositionStrategy();
@@ -13,6 +18,12 @@ public class NextPositionFactory {
 	private static INextPositionStrategy explorePositionStrategy = new ExplorePositionStrategy();
 
 
+	/**
+	 * Choose appropriate strategies to get the next position to go
+	 * @param car the carController to get the car status
+	 * @param mapRecorder the map to get position information
+	 * @return appropriate position strategy
+	 */
 	public static INextPositionStrategy chooseNextPositionStrategy(CarController car, MapRecorder mapRecorder) {
 		if (car.getHealth() < HEALTH_THRESHOLD && mapRecorder.hasHealthTrap()) {
 			return healPositionStrategy;
