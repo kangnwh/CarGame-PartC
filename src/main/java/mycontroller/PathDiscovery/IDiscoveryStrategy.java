963958c1a7014590b687f109301a3975f53a50d2
Nannan Gu <396426364@qq.com>
2018-05-25 20:46:48 +1000

    delete todo

--- src/main/java/mycontroller/PathDiscovery/IDiscoveryStrategy.java
+++ src/main/java/mycontroller/PathDiscovery/IDiscoveryStrategy.java
@@ -9,11 +9,17 @@ import java.util.LinkedList;
 
 /**
  * DiscoveryStrategy
- * <p>
- * Author Ning Kang
- * Date 24/5/18
+ * The interface of different strategies of find the path between two position.
+ * All path finder strategies should implement this interface.
  */
 
 public interface IDiscoveryStrategy {
+	/**
+	 * Find the path between two position according to the giving map
+	 * @param current current position of a car
+	 * @param target target position of a car
+	 * @param map map of all walls and known roads and traps
+	 * @return the coordinate linkedList representing the path of two positions
+	 */
 	LinkedList<Coordinate> findPath(Coordinate current, Coordinate target, MapTile[][] map);
 }
